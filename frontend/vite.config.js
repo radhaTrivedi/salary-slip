7f563d47834602ccbce9ff4fb76d0cff2c73ab9a
Radha Trivedi <radha17trivedi@gmail.com>
2026-09-25 10:20:31 +0530

    clean up index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
