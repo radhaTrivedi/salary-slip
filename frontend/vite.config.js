2d91e454267b1728d1833cd28929487256c246ca
Radha Trivedi <radha17trivedi@gmail.com>
2026-09-25 10:12:30 +0530

    add PWA support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
